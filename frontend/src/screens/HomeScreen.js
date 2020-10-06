25834c0f7c328716fdd5bf0d355be4497843d173
Edmund <munjyong@gmail.com>
2020-10-06 11:03:01 +0100

    Update products to fetch from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,25 @@
-import React from "react";
-import products from "../products";
-import Product from "../components/Product";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import axios from "axios";
 
 const HomeScreen = () => {
+  // State declaration in func. components
+  // products is the state name, setProducts is the function to manipulate state
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await axios.get("api/products");
+
+    setProducts(data);
+  };
+
+  // Runs as soon as the component loads
+  // Therefore the requests for the products should go inside
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = ({ match }) => {
-  // Matches the product id from products file to the product id in the URL
-  const product = products.find((product) => product._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  const fetchProduct = async () => {
+    const { data } = await axios.get(`/api/products/${match.params.id}`);
+
+    setProduct(data);
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
 
   return (
     <>
