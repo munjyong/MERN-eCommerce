272e7991c24686d6a4025fc0457871bbafe5d3d6
Edmund <munjyong@gmail.com>
2020-10-07 13:43:13 +0100

    Add MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
 
+// Enable environment variables
 dotenv.config();
+
+// Connect to DB
+connectDB();
 
 const app = express();
 
