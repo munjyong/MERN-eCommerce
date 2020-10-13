e12a9d61009a8c835d716db5049f56d309c23d6a
Edmund <munjyong@gmail.com>
2020-10-13 23:28:12 +0100

    Add controllers & refactor product routes

---
+++ backend/controllers/productController.js
@@ -0,0 +1,30 @@
+// asyncHandler is used to wrap async functions to catch errors instead of writing multiple try catch statements
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  PUBLIC
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+
+  res.json(products);
+});
+
+// @desc    Fetch a single product
+// @route   GET /api/products/:id
+// @access  PUBLIC
+const getProductById = asyncHandler(async (req, res) => {
+  // Finds product by grabbing the product id from URL
+  const product = await Product.findById(req.params.id);
+
+  // Check if product is found
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,40 +1,15 @@
 import express from "express";
-// asyncHandler is used to wrap async functions to catch errors instead of writing multiple try catch statements
-import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
+
+// Controllers
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  PUBLIC
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    // Load all products
-    const products = await Product.find({});
+router.route("/").get(getProducts);
 
-    res.json(products);
-  })
-);
-
-// @desc    Fetch a single product
-// @route   GET /api/products/:id
-// @access  PUBLIC
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    // Finds product by grabbing the product id from URL
-    const product = await Product.findById(req.params.id);
-
-    // Check if product is found
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.route("/:id").get(getProductById);
 
 export default router;
