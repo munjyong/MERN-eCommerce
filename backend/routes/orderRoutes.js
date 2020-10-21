2061e5b9b0e66e7f2fe6bd4f999642cbf7d09a4d
Edmund <munjyong@gmail.com>
2020-10-21 13:39:28 +0100

    Update order routes and controllers for PayPal

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
 
 // Controllers
-import { addOrderItems, getOrderById } from "../controllers/orderController.js";
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controllers/orderController.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -10,5 +14,7 @@ const router = express.Router();
 router.route("/").post(protect, addOrderItems);
 
 router.route("/:id").get(protect, getOrderById);
+
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 export default router;
